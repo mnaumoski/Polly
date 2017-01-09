61587dd39e0e41ede52f9f5c078c5196bd9cc036
annieg11 <aanchal.g.gupta@gmail.com>
2017-01-09 16:34:08 -0500

    template pie chart rendered function added to chart.js

--- client/views/partials/chart.js
+++ client/views/partials/chart.js
@@ -24,3 +24,48 @@ function builtPie() {
         y: 30,
         color: '#DF5353'
     });
+    $('#container-pie').highcharts({
+        
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        
+        title: {
+            text: ''
+        },
+        
+        credits: {
+            enabled: false
+        },
+        
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        
+        series: [{
+            type: 'pie',
+            name: 'Votes',
+            data: data
+        }]
+    });
+}
+
+/*
+ * Call the function to built the chart when the template is rendered
+ */
+Template.pieChart.rendered = function() {    
+    builtPie();
+}
