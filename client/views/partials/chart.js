0f9e665bb5c17a904519aeedb2c4d5b03052d6a1
annieg11 <aanchal.g.gupta@gmail.com>
2017-01-09 16:31:03 -0500

    builtpie function added to chart.js

--- client/views/partials/chart.js
+++ client/views/partials/chart.js
@@ -1,3 +1,26 @@
 // meteor add maazalik:highcharts : This package was used to get the Pie Chart.
 
-//  all the chart logic goes here
+//  all the chart logic goes here:
+
+function builtPie() {
+    
+    // 'external' data
+    var data = new Array();
+
+    data.push({
+        name: 'vote 0',
+        y: 10,
+        color: '#55BF3B'
+    });
+
+    data.push({
+        name: 'vote 1',
+        y: 12,
+        color: '#DDDF0D'
+    });
+
+    data.push({
+        name: 'vote 2',
+        y: 30,
+        color: '#DF5353'
+    });
