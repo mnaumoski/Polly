a49bd475845f44a2debecdb213630ad819e01f98
mnaumoski <naumoski.marija@gmail.com>
2017-01-13 15:29:06 -0500

    added photos to parallax

--- client/controllers/landing.js
+++ client/controllers/landing.js
@@ -1,14 +1,16 @@
-if (Meteor.isClient) {
-   Template.landing.onRendered(function(){
-       $(".parallax").parallax('enable');
-   });
-}
 
-if (Meteor.isServer) {
-   Meteor.startup(function () {
-       // code to run on server at startup
-        $(document).ready(function(){
-      $('.parallax').parallax('enable');
-    });
+   Template.landing.onRendered(function(){
+//       $('.parallax').parallax('enable');
+//     });
+       this.$(".parallax").parallax();
    });
-}
+
+
+// if (Meteor.isServer) {
+//    Meteor.startup(function () {
+//        // code to run on server at startup
+//         $(document).ready(function(){
+//       $('.parallax').parallax('enable');
+//     });
+//    });
+// }
