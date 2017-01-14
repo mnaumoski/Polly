d0fb5de080ca834624a9f3ea7baa6b619cb2d490
mnaumoski <naumoski.marija@gmail.com>
2017-01-14 13:43:11 -0500

    fixed mistake with parallx

--- client/controllers/landing.js
+++ client/controllers/landing.js
@@ -1,5 +1,16 @@
-   Template.userdashboard.onRendered(function(){
 
+   Template.landing.onRendered(function(){
+//       $('.parallax').parallax('enable');
+//     });
+       this.$(".parallax").parallax();
    });
 
 
+// if (Meteor.isServer) {
+//    Meteor.startup(function () {
+//        // code to run on server at startup
+//         $(document).ready(function(){
+//       $('.parallax').parallax('enable');
+//     });
+//    });
+// }
